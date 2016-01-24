04de4294f4134bf2afd5f20710580557b46944c1
Arpit Singla <arpit270896@gmail.com>
2016-01-24 22:26:07 +0530

    improves code

--- toogle-xinput.sh
+++ toogle-xinput.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 
+notify_timeout=5
+
 # $1=title, $2=message, $3=time, $4=exit_code
 function notify {
-	echo -e "$2"
 	de=$DESKTOP_SESSION
+	
 	if [[ $de = "plasma" ]]
 		then
-		kdialog --title "$1" --passivepopup "$2" $3 &
+		kdialog --title "$1" --passivepopup "$2" $notify_timeout &
 	fi
-	exit $4
+
+	echo -e "$2"
+	exit $3
 }
+
 
 # if no argument is passed
 if [ -z "$1" ]
 	then
-	notify "toogle-xinput.sh" "Invalid command.\nUsage: ./toogle-xinput.sh [id]" 5 1
+	notify "toogle-xinput.sh" "Invalid command.\nUsage: ./toogle-xinput.sh [id]" 1
 fi
 
 check_id="$(xinput list | grep "id=$1")"
@@ -23,7 +28,7 @@ check_id="$(xinput list | grep "id=$1")"
 # if there does not exist any input device corresponding to the given id
 if [ -z "$check_id" ]
 	then
-	notify "toogle-xinput.sh" "Invalid id.\nUse \"xinput list\" to see available ids." 5 1
+	notify "toogle-xinput.sh" "Invalid id.\nUse \"xinput list\" to see available ids." 1
 fi
 
 # get enabled status
@@ -36,8 +41,8 @@ pattern='((\w+\s+)+)\s+id=10'
 if [ $status -eq 0 ]
 	then
 	xinput enable $1
-	notify "toogle-xinput.sh" "Enabled Input: ${BASH_REMATCH[1]}" 5 0
+	notify "toogle-xinput.sh" "Enabled Input: ${BASH_REMATCH[1]}" 0
 else
 	xinput disable $1
-	notify "toogle-xinput.sh" "Disalbed Input: ${BASH_REMATCH[1]}" 5 0
+	notify "toogle-xinput.sh" "Disalbed Input: ${BASH_REMATCH[1]}" 0
 fi
